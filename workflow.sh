25900f7efcd8e725b7679fe5f80e9d4bf6d15f35
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-12-03 19:12:56 +0100

    saving files before refreshing line endings

--- workflow.sh
+++ workflow.sh
@@ -8,23 +8,33 @@ timestamp() {
 path=$(pwd)
 logfile="$path/$(filetimestamp)_log.txt"
 touch $logfile
+echo "Cloning project fra Github"
 echo "$(timestamp) : Cloning project from Github" >> $logfile
 echo "-----------------------------------------" >> $logfile
-git clone https://github.com/julian-code/makersofdenmark.git ./work-dir/
+git clone https://github.com/julian-code/makersofdenmark.git ./project/
+echo "Restoring project dependencies"
 echo "$(timestamp) : Restoring project dependencies" >> $logfile
 echo "-----------------------------------------" >> $logfile
-cd work-dir
-dotnet restores
+cd project
+dotnet restore >> $logfile
+echo "Running tests"
 echo "$(timestamp) : Running tests" >> $logfile
 echo "-----------------------------------------" >> $logfile
-dotnet test >> $logfile
+dotnet test --no-restore >> $logfile
+echo "Publishing"
 echo "$(timestamp) : Publishing" >> $logfile
 echo "-----------------------------------------" >> $logfile
-dotnet publish -o publish >> $logfile
+dotnet publish -o publish --no-restore >> $logfile
+echo "Zipping published files"
 echo "$(timestamp) : Zipping published files" >> $logfile
 echo "-----------------------------------------" >> $logfile
-zip ./publish ./publish.zip
+cd publish 
+zip ../../publish.zip ./* >> $logfile
+cd ..
+cd ..
+echo "Unzipping published files to newdir"
 echo "$(timestamp) : Publishing to new directory" >> $logfile
 echo "-----------------------------------------" >> $logfile
-
+mkdir newdir
+unzip publish.zip -d ./newdir >> $logfile
 echo "$(timestamp) : All Done" >> $logfile
